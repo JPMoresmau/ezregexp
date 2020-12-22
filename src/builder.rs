c608ecbe61a6efb6efc75a38a814e70281a41c1e
JP Moresmau <jp@moresmau.fr>
2020-12-22 14:21:59 +0100

    start/end

--- src/builder.rs
+++ src/builder.rs
@@ -1,5 +1,5 @@
-use std::fmt::{Display,Formatter, Result};
 use itertools::Itertools;
+use std::fmt::{Display, Formatter, Result};
 
 pub trait ToCode {
     fn to_code(&self) -> String;
@@ -11,61 +11,67 @@ pub enum Pattern {
     Text(String),
     Raw(String),
     Or(Vec<Pattern>),
-        Many{exp:Box<Pattern>,
+    Many {
+        exp: Box<Pattern>,
         low: u32,
         high: u32,
     },
     Digit,
+    InputStart,
+    InputEnd,
 }
 
 impl Display for Pattern {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match self {
-            Pattern::Sequence(v)=>v.iter().map(|e| {
-                match e {
-                    Pattern::Or(..) if v.len()>1=> write!(f, "({})", e),
-                    _=> write!(f, "{}", e),
+            Pattern::Sequence(v) => v
+                .iter()
+                .map(|e| match e {
+                    Pattern::Or(..) if v.len() > 1 => write!(f, "({})", e),
+                    _ => write!(f, "{}", e),
+                })
+                .collect(),
+            Pattern::Text(t) => write!(f, "{}", t),
+            Pattern::Raw(t) => write!(f, "{}", t),
+            Pattern::Or(v) => v
+                .iter()
+                .intersperse(&Pattern::Raw("|".to_owned()))
+                .map(|e| write!(f, "{}", e))
+                .collect::<Result>(),
+            Pattern::Many { exp, low, high } => {
+                let mut s = format!("{}", exp);
+                if s.len() > 2 || (s.len() == 2 && s.chars().into_iter().next().unwrap() != '\\') {
+                    s = format!("({})", s);
                 }
-            }).collect(),
-            Pattern::Text(t)=> write!(f, "{}", t),
-            Pattern::Raw(t)=> write!(f, "{}", t),
-            Pattern::Or(v)=> {
-                v.iter().intersperse(&Pattern::Raw("|".to_owned())).map(|e|{
-                    write!(f, "{}", e)
-                }).collect::<Result>()
-            },
-            Pattern::Many{exp,low,high}=>{
-                let mut s = format!("{}",exp);
-                if s.len()>2 || (s.len()==2 && s.chars().into_iter().next().unwrap() !='\\') {
-                    s=format!("({})",s);
+                match (low, high) {
+                    (0, 1) => write!(f, "{}?", s),
+                    (0, 0) => write!(f, "{}*", s),
+                    (1, 0) => write!(f, "{}+", s),
+                    (l, h) if l == h => write!(f, "{}{{{}}}", s, l),
+                    (l, h) => write!(f, "{}{{{},{}}}", s, l, h),
                 }
-                match (low,high){
-                    (0,1)=>write!(f,"{}?", s),
-                    (0,0)=>write!(f,"{}*", s),
-                    (1,0)=>write!(f,"{}+", s),
-                    (l,h)=>write!(f,"{}{{{},{}}}", s, l, h),
-                }
-
-            },
-            Pattern::Digit=>write!(f, r"\d"),
+            }
+            Pattern::Digit => write!(f, r"\d"),
+            Pattern::InputStart => write!(f, "^"),
+            Pattern::InputEnd => write!(f, "$"),
         }
     }
 }
 
 impl From<&str> for Pattern {
-    fn from(s:&str) -> Pattern {
+    fn from(s: &str) -> Pattern {
         Pattern::Text(s.to_owned())
     }
 }
 
 impl From<String> for Pattern {
-    fn from(s:String) -> Pattern {
+    fn from(s: String) -> Pattern {
         Pattern::Text(s)
     }
 }
 
 impl ToCode for Pattern {
-    fn to_code(&self) -> String{
+    fn to_code(&self) -> String {
         self.to_inner_code(CodeState::root())
     }
 }
@@ -76,94 +82,161 @@ struct CodeState {
 }
 
 impl CodeState {
-    fn root()-> Self {
-        CodeState{root:true,first:true}
+    fn root() -> Self {
+        CodeState {
+            root: true,
+            first: true,
+        }
     }
 
     fn first() -> Self {
-        CodeState{root:false,first:true}
+        CodeState {
+            root: false,
+            first: true,
+        }
     }
 
     fn next() -> Self {
-        CodeState{root:false, first: false}
+        CodeState {
+            root: false,
+            first: false,
+        }
     }
 }
 
 impl Pattern {
-
     fn to_inner_code(&self, state: CodeState) -> String {
         if state.first {
             match self {
-                Pattern::Text(txt) => if state.root {
-                    format!("text(\"{}\")", txt)
-                } else {
-                    format!("\"{}\"", txt)
-                },
+                Pattern::Text(txt) => {
+                    if state.root {
+                        format!("text(\"{}\")", txt)
+                    } else {
+                        format!("\"{}\"", txt)
+                    }
+                }
                 Pattern::Digit => "digit()".to_string(),
-                Pattern::Or(exps) => format!("either(({}))", exps.iter().map(|e| e.to_inner_code(CodeState::first())).join(", ")),
-                Pattern::Many { exp, low, high } => format!("{}.many({}, {})", exp.to_inner_code(CodeState::first()), low, high),
+                Pattern::Or(exps) => format!(
+                    "either(({}))",
+                    exps.iter()
+                        .map(|e| e.to_inner_code(CodeState::first()))
+                        .join(", ")
+                ),
+                Pattern::Many { exp, low, high } => format!(
+                    "{}.many({}, {})",
+                    exp.to_inner_code(CodeState::first()),
+                    low,
+                    high
+                ),
                 Pattern::Sequence(exps) => {
                     let mut s = String::new();
                     for e in exps {
                         if s.is_empty() {
-                            s.push_str(&format!("start_with({})", e.to_inner_code(CodeState::first())));
+                            match e {
+                                Pattern::InputStart => {
+                                    s.push_str(&e.to_inner_code(CodeState::first()))
+                                }
+                                _ => s.push_str(&format!(
+                                    "start_with({})",
+                                    e.to_inner_code(CodeState::first())
+                                )),
+                            };
                         } else {
                             s.push_str(&e.to_inner_code(CodeState::next()));
                         }
                     }
                     s
-                },
+                }
+                Pattern::InputStart => "at_start()".to_string(),
                 _ => String::new(),
             }
         } else {
             match self {
-                Pattern::Or(exps) => format!(".and_either(({}))",exps.iter().map(|e| e.to_inner_code(CodeState::first())).join(", ")),
-                Pattern::Many{exp,low,high} => {
-                    match (low,high){
-                        (0,1)=>format!(".and_maybe({})",exp.to_inner_code(CodeState::first())),
-                        (0,0)=>format!(".and_maybe_many({})",exp.to_inner_code(CodeState::first())),
-                        (1,0)=>format!(".and_many({})",exp.to_inner_code(CodeState::first())),
-                        _=>format!(".and_then({}).many({},{})",exp.to_inner_code(CodeState::first()),low,high),
-
-                    }
-                }
-                _ =>  format!(".and_then({})",self.to_inner_code(CodeState::first())),
+                Pattern::Or(exps) => format!(
+                    ".and_either(({}))",
+                    exps.iter()
+                        .map(|e| e.to_inner_code(CodeState::first()))
+                        .join(", ")
+                ),
+                Pattern::Many { exp, low, high } => match (low, high) {
+                    (0, 1) => format!(".and_maybe({})", exp.to_inner_code(CodeState::first())),
+                    (0, 0) => format!(".and_maybe_many({})", exp.to_inner_code(CodeState::first())),
+                    (1, 0) => format!(".and_many({})", exp.to_inner_code(CodeState::first())),
+                    (l, h) if l == h => format!(
+                        ".and_then({}).times({})",
+                        exp.to_inner_code(CodeState::first()),
+                        l
+                    ),
+                    _ => format!(
+                        ".and_then({}).many({},{})",
+                        exp.to_inner_code(CodeState::first()),
+                        low,
+                        high
+                    ),
+                },
+                Pattern::InputEnd => ".must_end()".to_string(),
+                _ => format!(".and_then({})", self.to_inner_code(CodeState::first())),
             }
         }
-
     }
 
-    pub fn and_either<PL:PatternList>(self, branches: PL)-> Self {
+    pub fn and_either<PL: PatternList>(self, branches: PL) -> Self {
         self.push(Pattern::Or(branches.into_patterns().collect()))
     }
 
-    pub fn and_then<T:Into<Pattern>>(self, exp:T) -> Self {
+    pub fn and_then<T: Into<Pattern>>(self, exp: T) -> Self {
         self.push(exp.into())
     }
 
-    pub fn and_maybe<T:Into<Pattern>>(self, exp:T) -> Self {
-        self.push(Pattern::Many{exp:Box::new(exp.into()),low:0,high:1})
+    pub fn and_maybe<T: Into<Pattern>>(self, exp: T) -> Self {
+        self.push(Pattern::Many {
+            exp: Box::new(exp.into()),
+            low: 0,
+            high: 1,
+        })
     }
 
-    pub fn and_maybe_many<T:Into<Pattern>>(self, exp:T) -> Self {
-        self.push(Pattern::Many{exp:Box::new(exp.into()),low:0,high:0})
+    pub fn and_maybe_many<T: Into<Pattern>>(self, exp: T) -> Self {
+        self.push(Pattern::Many {
+            exp: Box::new(exp.into()),
+            low: 0,
+            high: 0,
+        })
     }
 
-    pub fn and_many<T:Into<Pattern>>(self, exp:T) -> Self {
-        self.push(Pattern::Many{exp:Box::new(exp.into()),low:1,high:0})
+    pub fn and_many<T: Into<Pattern>>(self, exp: T) -> Self {
+        self.push(Pattern::Many {
+            exp: Box::new(exp.into()),
+            low: 1,
+            high: 0,
+        })
     }
 
     pub fn many(self, low: u32, high: u32) -> Self {
         match self {
-            Pattern::Sequence(mut exps) if exps.len()>0 => {
-                let e=exps.pop().unwrap();
-                exps.push(Pattern::Many{exp:Box::new(e),low:low,high:high});
+            Pattern::Sequence(mut exps) if exps.len() > 0 => {
+                let e = exps.pop().unwrap();
+                exps.push(Pattern::Many {
+                    exp: Box::new(e),
+                    low: low,
+                    high: high,
+                });
                 Pattern::Sequence(exps)
+            }
+            _ => Pattern::Many {
+                exp: Box::new(self),
+                low: low,
+                high: high,
             },
-            _ => Pattern::Many{exp:Box::new(self),low:low,high:high},
         }
-       
+    }
 
+    pub fn times(self, n: u32) -> Self {
+        self.many(n, n)
+    }
+
+    pub fn must_end(self) -> Self {
+        self.push(Pattern::InputEnd)
     }
 
     /*fn from_list(mut exprs: Vec<Pattern>) -> Pattern {
@@ -176,53 +249,63 @@ impl Pattern {
 
     fn push(self, p2: Pattern) -> Self {
         match self {
-            Pattern::Sequence(mut exps)=> {
+            Pattern::Sequence(mut exps) => {
                 exps.push(p2);
                 Pattern::Sequence(exps)
-            },
-            _ => Pattern::Sequence(vec![self,p2])
+            }
+            _ => Pattern::Sequence(vec![self, p2]),
         }
     }
 }
 
+pub fn at_start() -> Pattern {
+    Pattern::InputStart
+}
 
-
-pub fn start_with<T:Into<Pattern>>(exp:T)-> Pattern {
+pub fn start_with<T: Into<Pattern>>(exp: T) -> Pattern {
     exp.into()
 }
 
-pub fn text(text: &str)-> Pattern {
-   Pattern::Text(text.to_owned())
+pub fn text(text: &str) -> Pattern {
+    Pattern::Text(text.to_owned())
 }
 
 pub fn digit() -> Pattern {
     Pattern::Digit
 }
 
-pub fn either<PL:PatternList>(branches: PL)-> Pattern {
+pub fn either<PL: PatternList>(branches: PL) -> Pattern {
     Pattern::Or(branches.into_patterns().collect())
 }
 
-
-pub trait PatternList{
-    fn into_patterns(self) -> Box<dyn Iterator<Item=Pattern>>;
+pub trait PatternList {
+    fn into_patterns(self) -> Box<dyn Iterator<Item = Pattern>>;
 }
 
 impl PatternList for Vec<Pattern> {
-    fn into_patterns(self)-> Box<dyn Iterator<Item=Pattern>>{
+    fn into_patterns(self) -> Box<dyn Iterator<Item = Pattern>> {
         Box::new(self.into_iter())
     }
 }
 
-impl<T1,T2> PatternList for (T1,T2) where T1:Into<Pattern>,T2:Into<Pattern>, {
-    fn into_patterns(self)-> Box<dyn Iterator<Item=Pattern>>{
-        Box::new(vec![self.0.into(),self.1.into()].into_iter())
+impl<T1, T2> PatternList for (T1, T2)
+where
+    T1: Into<Pattern>,
+    T2: Into<Pattern>,
+{
+    fn into_patterns(self) -> Box<dyn Iterator<Item = Pattern>> {
+        Box::new(vec![self.0.into(), self.1.into()].into_iter())
     }
 }
 
-impl<T1,T2,T3> PatternList for (T1,T2,T3) where T1:Into<Pattern>,T2:Into<Pattern>,T3:Into<Pattern>,{
-    fn into_patterns(self)-> Box<dyn Iterator<Item=Pattern>>{
-        Box::new(vec![self.0.into(),self.1.into(), self.2.into()].into_iter())
+impl<T1, T2, T3> PatternList for (T1, T2, T3)
+where
+    T1: Into<Pattern>,
+    T2: Into<Pattern>,
+    T3: Into<Pattern>,
+{
+    fn into_patterns(self) -> Box<dyn Iterator<Item = Pattern>> {
+        Box::new(vec![self.0.into(), self.1.into(), self.2.into()].into_iter())
     }
 }
 
@@ -231,21 +314,66 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_basic_build(){
-        assert_eq!("Handel",text("Handel").to_string());
-        assert_eq!("gray|grey",either(("gray","grey")).to_string());
-        assert_eq!("gr(a|e)y",start_with("gr").and_either(("a","e")).and_then("y").to_string());
-        assert_eq!("colou?r",start_with("colo").and_maybe("u").and_then("r").to_string());
-        assert_eq!(r"\d{2,3}",digit().many(2, 3).to_string());
+    fn test_basic_build() {
+        assert_eq!("Handel", text("Handel").to_string());
+        assert_eq!("gray|grey", either(("gray", "grey")).to_string());
+        assert_eq!(
+            "gr(a|e)y",
+            start_with("gr")
+                .and_either(("a", "e"))
+                .and_then("y")
+                .to_string()
+        );
+        assert_eq!(
+            "colou?r",
+            start_with("colo").and_maybe("u").and_then("r").to_string()
+        );
+        assert_eq!(r"\d{2,3}", digit().many(2, 3).to_string());
+        assert_eq!(
+            r"^\d{4}-\d{2}-\d{2}$",
+            at_start()
+                .and_then(digit())
+                .times(4)
+                .and_then("-")
+                .and_then(digit())
+                .times(2)
+                .and_then("-")
+                .and_then(digit())
+                .times(2)
+                .must_end()
+                .to_string()
+        );
     }
 
     #[test]
-    fn test_basic_tocode(){
-        assert_eq!(r#"text("Handel")"#,text("Handel").to_code());
-        assert_eq!(r#"either(("gray", "grey"))"#,either(("gray","grey")).to_code());
-        assert_eq!(r#"start_with("gr").and_either(("a", "e")).and_then("y")"#,start_with("gr").and_either(("a","e")).and_then("y").to_code());
-        assert_eq!(r#"start_with("colo").and_maybe("u").and_then("r")"#,start_with("colo").and_maybe("u").and_then("r").to_code());
-        assert_eq!(r#"digit().many(2, 3)"#,digit().many(2, 3).to_code());
+    fn test_basic_tocode() {
+        assert_eq!(r#"text("Handel")"#, text("Handel").to_code());
+        assert_eq!(
+            r#"either(("gray", "grey"))"#,
+            either(("gray", "grey")).to_code()
+        );
+        assert_eq!(
+            r#"start_with("gr").and_either(("a", "e")).and_then("y")"#,
+            start_with("gr")
+                .and_either(("a", "e"))
+                .and_then("y")
+                .to_code()
+        );
+        assert_eq!(
+            r#"start_with("colo").and_maybe("u").and_then("r")"#,
+            start_with("colo").and_maybe("u").and_then("r").to_code()
+        );
+        assert_eq!(r#"digit().many(2, 3)"#, digit().many(2, 3).to_code());
+        assert_eq!(
+            r#"at_start().and_then(digit()).times(4).and_then("-").and_then(digit()).times(2).and_then("-").and_then(digit()).times(2).must_end()"#,
+            at_start()
+                .and_then(digit().times(4))
+                .and_then("-")
+                .and_then(digit().times(2))
+                .and_then("-")
+                .and_then(digit().times(2))
+                .must_end()
+                .to_code()
+        );
     }
 }
-
--- src/parser.rs
+++ src/parser.rs
@@ -1,54 +1,73 @@
-use regex_syntax::ast::{Alternation, Ast, Class, ClassPerl, ClassPerlKind, Concat, Error, Group, Literal, Repetition, RepetitionKind, RepetitionOp, RepetitionRange, parse::Parser};
-use crate::builder::{Pattern};
+use crate::builder::Pattern;
+use regex_syntax::ast::{
+    parse::Parser, Alternation, Assertion, AssertionKind, Ast, Class, ClassPerl, ClassPerlKind,
+    Concat, Error, Group, Literal, Repetition, RepetitionKind, RepetitionOp, RepetitionRange,
+};
 
-pub fn explain(regex: &str) -> Result<Pattern,Error> {
-    let mut p=Parser::new();
-    p.parse(regex).and_then(|a| { println!("ast: {:?}",a); do_explain(&a)})
-
+pub fn explain(regex: &str) -> Result<Pattern, Error> {
+    let mut p = Parser::new();
+    p.parse(regex).and_then(|a| {
+        println!("ast: {:?}", a);
+        do_explain(&a)
+    })
 }
 
-fn do_explain(ast: &Ast) -> Result<Pattern,Error> {
-   
+fn do_explain(ast: &Ast) -> Result<Pattern, Error> {
     match ast {
-        Ast::Concat(Concat{asts,..})=>
-            Ok(simplify(asts.iter().map(|a| do_explain(a)).collect::<Result<Vec<Pattern>,Error>>()?))
-        ,
-        Ast::Literal(Literal{c,..}) =>
-            Ok(Pattern::Text(format!("{}",c)))
-        ,
-        Ast::Alternation(Alternation{asts,..})=>
-            Ok(Pattern::Or(asts.iter().map(|a| do_explain(a)).collect::<Result<Vec<Pattern>,Error>>()?))
-        ,
-        Ast::Group(Group{ast,..})=>
-            do_explain(ast),
-        Ast::Repetition(Repetition{ast,op,..})=>{
-            let bds=bounds(op);
-            Ok(Pattern::Many{exp:Box::new(do_explain(ast)?),low:bds.0,high:bds.1})
-        },
-        Ast::Class(Class::Perl(ClassPerl{ kind:ClassPerlKind::Digit, ..}))   =>Ok(Pattern::Digit),
+        Ast::Concat(Concat { asts, .. }) => Ok(simplify(
+            asts.iter()
+                .map(|a| do_explain(a))
+                .collect::<Result<Vec<Pattern>, Error>>()?,
+        )),
+        Ast::Literal(Literal { c, .. }) => Ok(Pattern::Text(format!("{}", c))),
+        Ast::Alternation(Alternation { asts, .. }) => Ok(Pattern::Or(
+            asts.iter()
+                .map(|a| do_explain(a))
+                .collect::<Result<Vec<Pattern>, Error>>()?,
+        )),
+        Ast::Group(Group { ast, .. }) => do_explain(ast),
+        Ast::Repetition(Repetition { ast, op, .. }) => {
+            let bds = bounds(op);
+            Ok(Pattern::Many {
+                exp: Box::new(do_explain(ast)?),
+                low: bds.0,
+                high: bds.1,
+            })
+        }
+        Ast::Class(Class::Perl(ClassPerl {
+            kind: ClassPerlKind::Digit,
+            ..
+        })) => Ok(Pattern::Digit),
+        Ast::Assertion(Assertion {
+            kind: AssertionKind::StartLine,
+            ..
+        }) => Ok(Pattern::InputStart),
+        Ast::Assertion(Assertion {
+            kind: AssertionKind::EndLine,
+            ..
+        }) => Ok(Pattern::InputEnd),
         _ => Ok(Pattern::Raw(String::new())),
     }
 }
 
-fn bounds(op: &RepetitionOp) -> (u32,u32){
+fn bounds(op: &RepetitionOp) -> (u32, u32) {
     match &op.kind {
-        RepetitionKind::ZeroOrOne => (0,1),
-        RepetitionKind::ZeroOrMore => (0,0),
-        RepetitionKind::OneOrMore => (1,0),
+        RepetitionKind::ZeroOrOne => (0, 1),
+        RepetitionKind::ZeroOrMore => (0, 0),
+        RepetitionKind::OneOrMore => (1, 0),
         RepetitionKind::Range(r) => match r {
-            RepetitionRange::AtLeast(m)=>(*m ,0),
-            RepetitionRange::Exactly(m)=>(*m ,*m),
-            RepetitionRange::Bounded(l,h)=>(*l ,*h),
-        }
-        
+            RepetitionRange::AtLeast(m) => (*m, 0),
+            RepetitionRange::Exactly(m) => (*m, *m),
+            RepetitionRange::Bounded(l, h) => (*l, *h),
+        },
     }
 }
 
-fn simplify(exps:Vec<Pattern>) -> Pattern {
-    let mut nexps=vec![];
+fn simplify(exps: Vec<Pattern>) -> Pattern {
+    let mut nexps = vec![];
     for p in exps.into_iter() {
-        if let Pattern::Text(t) = p{
-            let op0=nexps.pop();
+        if let Pattern::Text(t) = p {
+            let op0 = nexps.pop();
             if let Some(Pattern::Text(mut t0)) = op0 {
                 t0.push_str(&t);
                 nexps.push(Pattern::Text(t0));
@@ -60,10 +79,9 @@ fn simplify(exps:Vec<Pattern>) -> Pattern {
             }
         } else {
             nexps.push(p);
-        
         }
     }
-    if nexps.len()==1{
+    if nexps.len() == 1 {
         nexps.pop().unwrap()
     } else {
         Pattern::Sequence(nexps)
@@ -91,7 +109,7 @@ impl Default for ExplainState {
 
 impl ExplainState {
     fn string(&mut self) -> bool {
-        if let Some(State::String) = self.stack.last(){ 
+        if let Some(State::String) = self.stack.last(){
             return false;
         } else {
             self.stack.push(State::String);
@@ -100,7 +118,7 @@ impl ExplainState {
     }
 
     fn unstring(&mut self) -> bool {
-        if let Some(State::String) = self.stack.last(){ 
+        if let Some(State::String) = self.stack.last(){
             self.stack.pop();
             return true;
         }
@@ -108,7 +126,7 @@ impl ExplainState {
     }
 
     fn is_root(&self) -> bool {
-        if let Some(State::Root) = self.stack.last(){ 
+        if let Some(State::Root) = self.stack.last(){
             return true;
         }
         false
@@ -122,11 +140,27 @@ mod tests {
     use crate::builder::ToCode;
 
     #[test]
-    fn test_basic_explain(){
-        assert_eq!(Ok(r#"text("Handel")"#.to_owned()),explain("Handel").map(|p| p.to_code()));
-        assert_eq!(Ok(r#"either(("gray", "grey"))"#.to_owned()),explain("gray|grey").map(|p| p.to_code()));
-        assert_eq!(Ok(r#"start_with("gr").and_either(("a", "e")).and_then("y")"#.to_owned()),explain("gr(a|e)y").map(|p| p.to_code()));
-        assert_eq!(Ok(r#"start_with("colo").and_maybe("u").and_then("r")"#.to_owned()),explain("colou?r").map(|p| p.to_code()));
-        assert_eq!(Ok(r#"digit().many(2, 3)"#.to_owned()),explain(r#"\d{2,3}"#).map(|p| p.to_code()));
+    fn test_basic_explain() {
+        assert_eq!(
+            Ok(r#"text("Handel")"#.to_owned()),
+            explain("Handel").map(|p| p.to_code())
+        );
+        assert_eq!(
+            Ok(r#"either(("gray", "grey"))"#.to_owned()),
+            explain("gray|grey").map(|p| p.to_code())
+        );
+        assert_eq!(
+            Ok(r#"start_with("gr").and_either(("a", "e")).and_then("y")"#.to_owned()),
+            explain("gr(a|e)y").map(|p| p.to_code())
+        );
+        assert_eq!(
+            Ok(r#"start_with("colo").and_maybe("u").and_then("r")"#.to_owned()),
+            explain("colou?r").map(|p| p.to_code())
+        );
+        assert_eq!(
+            Ok(r#"digit().many(2, 3)"#.to_owned()),
+            explain(r#"\d{2,3}"#).map(|p| p.to_code())
+        );
+        assert_eq!(Ok(r#"at_start().and_then(digit()).times(4).and_then("-").and_then(digit()).times(2).and_then("-").and_then(digit()).times(2).must_end()"#.to_owned()),explain(r"^\d{4}-\d{2}-\d{2}$").map(|p| p.to_code()));
     }
 }
